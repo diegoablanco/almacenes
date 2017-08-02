09dd6bcd09a6de05d0e91691e2029b5c0c56b77d
Diego Blanco <diegoablanco@gmail.com>
2017-08-02 19:02:18 -0300

    hot loader customers

---
+++ server/services/customers/customers.hooks.js
@@ -0,0 +1,35 @@
+const auth = require('feathers-authentication').hooks;
+
+module.exports = {
+  before: {
+    all: [
+      auth.authenticate(['jwt', 'local'])
+    ],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+};
