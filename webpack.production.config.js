a14232526c49e94897f4413ed5e5a89f0830a3fd
Diego <diegoablanco@gmail.com>
2018-02-21 12:51:37 -0300

    trying to make styling work in prod

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -6,12 +6,17 @@
 // Changes needed if used for devserver mode.
 
 const webpack = require('webpack');
-const rucksack = require('rucksack-css');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const envalid = require('envalid');
 const path = require('path');
 const RewriteImportPlugin = require("less-plugin-rewrite-import")
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const extractLess = new ExtractTextPlugin({
+  filename: '[name].[contenthash].css'
+  // disable: process.env.NODE_ENV === 'development'
+})
 const ROOT_DIR = path.resolve(__dirname);
 const NODE_MODULES_DIR = path.resolve(__dirname, 'node_modules');
 
@@ -27,9 +32,9 @@ if (config.NODE_ENV === 'devserver') {
 const isProduction = config.isProduction;
 
 const outputPublicPaths = {
-  production: 'public/dist/',
+  production: 'server/public/dist/',
   development: '/dist/',
-  devserver: 'http://localhost:8080/', // we don't use this config for webpack-dev-server
+  devserver: 'http://localhost:8080/' // we don't use this config for webpack-dev-server
 };
 
 console.log(`----- ${config.NODE_ENV.toUpperCase()} build.`); // eslint-disable-line no-console
@@ -40,56 +45,7 @@ const webpackConfig = {
   // re devtool: http://cheng.logdown.com/posts/2016/03/25/679045
   devtool: isProduction ? 'cheap-module-source-map' : 'source-map',
   entry: {
-    main: ['./index.js'],
-    // Webpack cannot produce chunks with a stable chunk hash as of June 2016,
-    // stable meaning the hash value changes only when the the code itself changes.
-    // See doc/FAQ.md#webpackChunks.
-    // This vendor chunk will not reduce network requests, it will likely force a second request
-    // each time the main chunk changes. So why separate them?
-    /*
-    vendor: [ // review these
-      'feathers-client',
-      'feathers-hooks-common',
-      'feathers-reduxify-authentication',
-      'feathers-reduxify-services',
-      'feathers-service-verify-reset',
-      'material-ui',
-      'react',
-      'react-dom',
-      'react-redux',
-      'react-router',
-      'react-router-redux',
-      'react-tap-event-plugin',
-      'redux',
-      'redux-actions',
-      'redux-auth-wrapper',
-      'redux-form',
-      'redux-form-material-ui',
-      'redux-multi',
-    ],
-    */
-    // Chunks for code which is dynamically optionally loaded makes sense.
-    // The first page will render faster as the parsing of such chunks can be delayed
-    // till they are needed.
-    // Of course the React routing must be changed to load such chunks as needed.
-    // Maybe we'll make the routing do that at some time.
-    /*
-    user: [
-      // './screens/Users/UserSignIn', // sign in occurs often enough to retain in main chunk
-      './screens/Users/UserEmailChange',
-      './screens/Users/UserForgotPwdReset',
-      './screens/Users/UserForgotPwdSendEmail',
-      './screens/Users/UserPasswordChange',
-      './screens/Users/UserProfile',
-      './screens/Users/UserProfileChange',
-      './screens/Users/UserRolesChange',
-      './screens/Users/UserSignIn',
-      './screens/Users/UserSignInPending',
-      './screens/Users/UserSignUp',
-      './screens/Users/UserSignUpSendEmail',
-      './screens/Users/UserSignUpValidateEmail',
-    ],
-    */
+    main: ['./index.js']
   },
   output: {
     filename: '[name].bundle.[chunkhash].js',
@@ -154,27 +110,20 @@ const webpackConfig = {
     // Tell Webpack where it should store the resulting code.
     path: path.join(__dirname, 'public', 'dist'),
     // Give Webpack the URL that points the server to output.path
-    publicPath: outputPublicPaths[config.NODE_ENV],
+    publicPath: outputPublicPaths[config.NODE_ENV]
   },
-  /* This is needed for joi to work on the browser, if the client has that dependency
-  node: {
-    net: 'empty',
-    tls: 'empty',
-    dns: 'empty',
-  },
-  */
   module: {
     loaders: [
       {
         // File index.html is created by html-webpack-plugin. It should be a file webpack processes.
         test: /\.html$/,
-        loader: 'file?name=[name].[ext]',
+        loader: 'file?name=[name].[ext]'
       },
       {
         // When require'd, these /client/../*.inject.css files are injected into the DOM as is.
         test: /\.inject\.css$/,
         include: /client/,
-        loader: 'style!css',
+        loader: 'style!css'
       },
       {
         // When required, the class names in these /client/../*.css are returned as an object.
@@ -184,15 +133,15 @@ const webpackConfig = {
         loaders: [
           'style-loader',
           'css-loader?modules&sourceMap&importLoaders=1&localIdentName=' +
-          '[name]__[local]___[hash:base64:5]',
+          '[name]__[local]___[hash:base64:5]'
           //'postcss-loader',
-        ],
+        ]
       },
       {
         // Standard processing for .css outside /client
         test: /\.css$/,
         exclude: /client/,
-        loader: 'style!css',
+        loader: 'style!css'
       },
       {
         test: /\.(js|jsx)$/, // does anyone still use .jsx?
@@ -201,30 +150,32 @@ const webpackConfig = {
           /*
           'react-hot',
           */
-          'babel-loader',
-        ],
-      },     
+          'babel-loader'
+        ]
+      },
       {
-          test: /\.less/,
+        test: /\.less$/,
+        use: extractLess.extract({
+          publicPath: '../',
           use: [{
-              loader: "style-loader"
+            loader: 'css-loader'
           }, {
-              loader: "css-loader", options: {
-                  sourceMap: true
-              }
-          }, {
-              loader: "less-loader", options: 
+            loader: 'less-loader',
+            options:
               {
                 paths: [ROOT_DIR, NODE_MODULES_DIR], // this will force less-loader to use its own resolver, both should be absolute path
                 plugins: [
                   new RewriteImportPlugin({
                     paths: {
-                      '../../theme.config':  __dirname + '/client/semantic-ui/theme.config',
-                    },
-                  }),
-                ],
+                      '../../theme.config': `${__dirname}/client/semantic-ui/theme.config`
+                    }
+                  })
+                ]
               }
-          }]
+          }],
+          // use style-loader in development
+          fallback: 'style-loader'
+        })
       },
       {
         test: /\.jpe?g$|\.gif$|\.png$|\.ttf$|\.eot$|\.svg$/,
@@ -234,7 +185,7 @@ const webpackConfig = {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'url-loader?limit=10000&mimetype=application/fontwoff'
       }
-    ],
+    ]
   },
   resolve: {
     extensions: ['.js', '.jsx'],
@@ -271,18 +222,18 @@ const webpackConfig = {
        'react-redux$': path.join(__dirname, 'node_modules', 'react-redux', 'dist',
          (isProduction ? 'react-redux.min.js' : 'react-redux.js')),
        */
-    },
+    }
   },
   // postcss: [
-    // rucksack({
-      // autoprefixer: true,
-    // }),
+  // rucksack({
+  // autoprefixer: true,
+  // }),
   // ],
   plugins: [
     // Webpack's default file watcher does not work with NFS file systems on VMs,
     // definitely not with Oracle VM, and likely not with other VMs.
     // OldWatchingPlugin is a slower alternative that works everywhere.
-    //new webpack.OldWatchingPlugin(), // can use "webpack-dev-server --watch-poll" instead
+    // new webpack.OldWatchingPlugin(), // can use "webpack-dev-server --watch-poll" instead
     /*
      Build our HTML file.
      */
@@ -297,7 +248,7 @@ const webpackConfig = {
         conservativeCollapse: true,
         minifyCSS: true,
         minifyJS: true,
-        preserveLineBreaks: true, // leave HTML readable
+        preserveLineBreaks: true // leave HTML readable
       },
       cache: false,
       /* We'd need this if we had a dynamically loaded user chunk
@@ -319,24 +270,15 @@ const webpackConfig = {
       copyWindowVars: {},
       scripts: ['/socket.io/socket.io.js'],
       devServer: false,
-      googleAnalytics: false,
+      googleAnalytics: false
     }),
     // Define replacements for global constants in the client code.
     new webpack.DefinePlugin({
       'process.env': { NODE_ENV: JSON.stringify(config.NODE_ENV) }, // used by React, etc
-      __processEnvNODE_ENV__: JSON.stringify(config.NODE_ENV), // used by us
+      __processEnvNODE_ENV__: JSON.stringify(config.NODE_ENV) // used by us
     }),
-    /* We'd need this if we had a vendor chunk
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      // filename: 'vendor.bundle.[chunkhash].js',
-      minChunks: Infinity,
-    }),
-    */
-    /* Trying to get a stable chunk hash. Sometimes works in particular circumstances.
-    new webpack.optimize.OccurrenceOrderPlugin(true),
-     */
-  ],
+    extractLess
+  ]
   /* Trying to get a stable chunk hash
    recordsPath: path.join(__dirname, 'webpack.records.json'),
    */
@@ -347,7 +289,7 @@ const webpackConfig = {
     hot: true
   },
   */
-};
+}
 
 // Production customization
 
@@ -358,14 +300,14 @@ if (isProduction) {
      for production if you choose not to use their run-time versions.
      */
     new UglifyJsPlugin({
-	  sourceMap: true,
-	  uglifyOptions: {
-		ecma:8,  
-		compress: {
+	 sourceMap: true,
+	 uglifyOptions: {
+        ecma:8,
+        compress: {
 		  warnings: false
-		}
+        }
 	  }
-	})
+    })
   );
 }
 
@@ -383,7 +325,7 @@ function validateEnvironmentVariables() {
     dev: 'development',
     devserver: 'devserver',
     testing: 'devserver',
-    test: 'devserver',
+    test: 'devserver'
   };
 
   const cleanEnv = envalid.cleanEnv(process.env,
@@ -391,198 +333,10 @@ function validateEnvironmentVariables() {
       NODE_ENV: strPropType({
         choices: Object.keys(nodeEnv),
         default: 'developmwent',
-        desc: 'processing environment',
-      }),
+        desc: 'processing environment'
+      })
     }
   );
 
   process.env.NODE_ENV = nodeEnv[cleanEnv.NODE_ENV];
 }
-
-/*
-The most obvious use of Webpack Dll's is to reduce the build time.
-We decided in our case that the improved performance wasn't worth the increased complexity.
-
-However Dll's were also a potential approach to producing stable chunk hashes.
-We were unable to get that to work in the edge cases.
-
-I'm keeping this info here in case we have to revisit the topic, so as to have a starting point.
-
-//robertknight.github.io/posts/webpack-dll-plugins/
-//medium.com/@soederpop/webpack-plugins-been-we-been-keepin-on-the-dll-cdfdd6cb8cd7#.5dnhzui7n
-
-without Dll
- 73747ms build modules
- 249ms seal
- 566ms optimize
- 167ms hashing
- 280ms create chunk assets
- 14ms additional chunk assets
- 9453ms optimize chunk assets
- 347ms optimize assets
- 928ms emit
- Hash: 2dfc2b299050a96627b4
- Version: webpack 1.13.1
- Time: 85894ms
-
- Asset                                   Size          Chunks        Chunk Names
- user.bundle.a8a96d270291ce167d6c.js      639 kB       0  [emitted]  user
- vendor.bundle.95e8a535b85b002ff2eb.js   9.89 MB       1  [emitted]  vendor
- main.bundle.27c3a93a890af488d77e.js     1.12 MB       2  [emitted]  main
- index.html                              1.13 kB          [emitted]
-
-
- dll
- [ 'fixed-data-table',
- 'isomorphic-fetch',
- 'joi',
- 'material-ui',
- 'react',
- 'react-dom',
- 'react-redux',
- 'react-router',
- 'react-router-proxy-loader',
- 'react-router-redux',
- 'react-tap-event-plugin',
- 'redux',
- 'redux-actions',
- 'redux-auth-wrapper',
- 'redux-form',
- 'redux-logger',
- 'redux-multi',
- 'redux-promise-middleware',
- 'redux-thunk' ]
- Hash: 6362e9e1a2e83a53043b
- Version: webpack 1.13.1
- Time: 23239ms
-
- Asset                                  Size          Chunks        Chunk Names
- vendor.bundle.15ebd92af32b28ad8e50.js  8.65 MB       0  [emitted]  vendor
- + 727 hidden modules
-
-with Dll
- 38933ms build modules
- 144ms seal
- 123ms optimize
- 186ms hashing
- 472ms create chunk assets
- 25ms additional chunk assets
- 8762ms optimize chunk assets
- 777ms optimize assets
- 367ms emit
- Hash: e577b3f7fb9ce7b7d8ca
- Version: webpack 1.13.1
- Time: 49977ms
-
- Asset                                   Size       Chunks        Chunk Names
- main.bundle.407930e583af7eea79c4.js  5.69 MB       0  [emitted]  main
- user.bundle.0f13a46c5f30ec4a2ea2.js   4.6 MB       1  [emitted]  user
- index.html  1.02 kB          [emitted]
-
-
--- stats while running 2nd time, so cache is primed
-
-build regular without a dll
- 50818ms build modules
- 219ms seal
- 304ms optimize
- 121ms hashing
- 640ms create chunk assets
- 41ms additional chunk assets
- 12313ms optimize chunk assets
- 1394ms optimize assets
- 891ms emit
- Hash: 2dfc2b299050a96627b4
- Version: webpack 1.13.1
- Time: 67101ms
-
- Asset     Size  Chunks             Chunk Names
- user.bundle.a8a96d270291ce167d6c.js   639 kB       0  [emitted]  user
- vendor.bundle.95e8a535b85b002ff2eb.js  9.89 MB       1  [emitted]  vendor
- main.bundle.27c3a93a890af488d77e.js  1.12 MB       2  [emitted]  main
- index.html  1.13 kB          [emitted]
-
-build the dll
- [ 'fixed-data-table',
- 'isomorphic-fetch',
- 'joi',
- 'material-ui',
- 'react',
- 'react-dom',
- 'react-redux',
- 'react-router',
- 'react-router-proxy-loader',
- 'react-router-redux',
- 'react-tap-event-plugin',
- 'redux',
- 'redux-actions',
- 'redux-auth-wrapper',
- 'redux-form',
- 'redux-logger',
- 'redux-multi',
- 'redux-promise-middleware',
- 'redux-thunk' ]
- Hash: 6362e9e1a2e83a53043b
- Version: webpack 1.13.1
- Time: 34815ms
-
- Asset     Size  Chunks             Chunk Names
- vendor.bundle.15ebd92af32b28ad8e50.js  8.65 MB       0  [emitted]  vendor
- + 727 hidden modules
-
-build with dll
- 28726ms build modules
- 71ms seal
- 31ms optimize
- 72ms hashing
- 142ms create chunk assets
- 4ms additional chunk assets
- 5790ms optimize chunk assets
- 582ms optimize assets
- 396ms emit
- Hash: e577b3f7fb9ce7b7d8ca
- Version: webpack 1.13.1
- Time: 35950ms
-
- Asset     Size  Chunks             Chunk Names
- main.bundle.407930e583af7eea79c4.js  5.69 MB       0  [emitted]  main
- user.bundle.0f13a46c5f30ec4a2ea2.js   4.6 MB       1  [emitted]  user
- index.html  1.02 kB          [emitted]
- */
-
-// interesting stuff
-// github.com/gaearon/react-hot-boilerplate/pull/61 BerndWessels Jun 14
-
-/*
-{
-  // ASSET FONT LOADER
-  // Reference: https://github.com/webpack/file-loader
-  // Copy png, jpg, jpeg, gif, svg, woff, woff2, ttf, eot files to output
-  // Rename the file using the asset hash
-  // Pass along the updated reference to your code
-  // You can add here any file extension you want to get copied to your output
-  test: /\.(svg|woff|woff2|ttf|eot)$/,
-    loader: 'file?name=assets/fonts/[name].[hash].[ext]'
-}, {
-  // ASSET IMAGE LOADER
-  // Reference: https://github.com/webpack/file-loader
-  // Copy png, jpg, jpeg, gif, svg, woff, woff2, ttf, eot files to output
-  // Rename the file using the asset hash
-  // Pass along the updated reference to your code
-  // You can add here any file extension you want to get copied to your output
-  test: /\.(png|jpg|jpeg|gif)$/,
-    loader: 'file?name=assets/images/[name].[hash].[ext]'
-}, {
-  // HTML LOADER
-  // Reference: https://github.com/webpack/raw-loader
-  // Allow loading html through js
-  test: /\.html$/,
-    loader: 'html'
-}, {
-  // JSON LOADER
-  // Reference: https://github.com/webpack/json-loader
-  // Allow loading JSON
-  test: /\.json$/,
-    loader: 'json'
-}
-*/
