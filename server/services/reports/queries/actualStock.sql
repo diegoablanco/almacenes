2db901a2ad9737d4bb99147a3ad33c3132d2fb31
diego <diegoablanco@gmail.com>
2018-10-17 11:03:31 -0300

    replace SLQ cross apply

--- server/services/reports/queries/actualStock.sql
+++ server/services/reports/queries/actualStock.sql
@@ -6,17 +6,12 @@ select
 from products p
     join [dbo].[stockAccountMovements] sam on p.stockAccountMovementId = sam.id
     join [dbo].[productTypes] pt on p.typeId = pt.id
-    inner join (
-        select p2.stockAccountMovementId, stockAccountMovements.type
-    from products p
-        cross apply (select top 1
-            *
+    join (
+		select p2.code, max(p2.stockAccountMovementId) as stockAccountMovementId
         from products p2
-        where p.code = p2.code
-        order by createdAt desc) p2
         join stockAccountMovements on stockAccountMovements.id = p2.stockAccountMovementId
-    group by p2.stockAccountMovementId, stockAccountMovements.type
-    having stockAccountMovements.type = 'receive'
+		group by p2.code
     ) lastProductMovement on lastProductMovement.stockAccountMovementId = p.stockAccountMovementId
+	and lastProductMovement.code = p.code
 where (:dateTo is null OR sam.date <= :dateTo)
 order by pt.ean
