97056ee448d6c2e6aaca0d931123c1fba524071a
diego <diegoablanco@gmail.com>
2018-10-18 12:43:58 -0300

    added report query added report template

---
+++ server/services/reports/queries/dailyStockValue.sql
@@ -0,0 +1,25 @@
+select
+  FORMAT(sam.createdAt,  'dd/MM/yyyy', 'en-US') AS 'Fecha',
+  count(p.id) as 'Cantidad',
+  FORMAT(avg(COALESCE(nullif(p.price, 0), pt.price, 0)), 'C', 'es-es') as 'Precio promedio'
+from
+  products p
+  join [dbo].[stockAccountMovements] sam on p.stockAccountMovementId = sam.id
+  join [dbo].[productTypes] pt on p.typeId = pt.id
+  join (
+    select p2.code, max(p2.stockAccountMovementId) as stockAccountMovementId
+      from products p2
+      join stockAccountMovements on stockAccountMovements.id = p2.stockAccountMovementId
+		  group by p2.code
+    ) lastProductMovement on lastProductMovement.stockAccountMovementId = p.stockAccountMovementId
+	  and lastProductMovement.code = p.code
+where
+  (
+    :dateTo is null
+    OR sam.date <= :dateTo
+  )
+and sam.type = 'receive'
+group by
+  FORMAT(sam.createdAt,  'dd/MM/yyyy', 'en-US')
+order by 
+  FORMAT(sam.createdAt,  'dd/MM/yyyy', 'en-US')
--- server/services/reports/queries/index.js
+++ server/services/reports/queries/index.js
@@ -5,11 +5,15 @@ const actualStock = fs.readFileSync(path.resolve(__dirname, 'actualStock.sql')).
 const issuedStock = fs.readFileSync(path.resolve(__dirname, 'issuedStock.sql')).toString()
 const receivedStock = fs.readFileSync(path.resolve(__dirname, 'receivedStock.sql')).toString()
 const stockCount = fs.readFileSync(path.resolve(__dirname, 'stockCount.sql')).toString()
+const dailyStockValue = fs.readFileSync(path.resolve(__dirname, 'dailyStockValue.sql')).toString()
+const monthlyStockValue = fs.readFileSync(path.resolve(__dirname, 'monthlyStockValue.sql')).toString()
 
 module.exports = {
   receivedStock,
   issuedStock,
   actualStock,
-  stockCount
+  stockCount,
+  dailyStockValue,
+  monthlyStockValue
 }
 
