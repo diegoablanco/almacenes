ae82aa291d44fcff2a6eba3ea733d10a391f8b53
diego <diegoablanco@gmail.com>
2018-10-18 12:44:20 -0300

    add report type support add stock value report

--- server/services/reports/service.js
+++ server/services/reports/service.js
@@ -5,7 +5,7 @@ const path = require('path')
 const hooks = require('./hooks')
 const getDatabase = require('../../../server/database')
 const xlsx = require('xlsx')
-const { receivedStock, issuedStock, actualStock, stockCount } = require('./queries')
+const { receivedStock, issuedStock, actualStock, stockCount, dailyStockValue, monthlyStockValue } = require('./queries')
 const { writeArrayToSheet } = require('./helpers')
 
 
@@ -19,31 +19,54 @@ module.exports = function () {
     {
       async create({ reportType, options }) {
         const sequelize = getDatabase()
-        const replacements = {
-          dateFrom: options.dateFrom || null,
-          dateTo: options.dateTo || moment().endOf('day').toDate(),
-          receipt: options.receipt || null
-        }
         try {
-          const [receivedStocks] = await sequelize.query(receivedStock, { replacements })
-          const [issuedStocks] = await sequelize.query(issuedStock, { replacements })
-          const [actualStocks] = await sequelize.query(actualStock, { replacements })
-          const [stockCounts] = await sequelize.query(stockCount, { replacements })
+          switch (reportType) {
+            case 'stock':
+            {
+              const replacements = {
+                dateFrom: options.dateFrom || null,
+                dateTo: options.dateTo || moment().endOf('day').toDate(),
+                receipt: options.receipt || null
+              }
+              const [receivedStocks] = await sequelize.query(receivedStock, { replacements })
+              const [issuedStocks] = await sequelize.query(issuedStock, { replacements })
+              const [actualStocks] = await sequelize.query(actualStock, { replacements })
+              const [stockCounts] = await sequelize.query(stockCount, { replacements })
 
-          return {
-            sheets: [receivedStocks, issuedStocks, actualStocks, stockCounts],
-            fileName: `Reporte de Stock ${replacements.dateFrom ? `del ${moment(replacements.dateFrom).format('D-MM-YYYY')}` : ''} al ${moment(replacements.dateTo).format('D-MM-YYYY')}.xlsx`
+              return {
+                sheets: [receivedStocks, issuedStocks, actualStocks, stockCounts],
+                fileName: `Reporte de Stock ${replacements.dateFrom ? `del ${moment(replacements.dateFrom).format('D-MM-YYYY')}` : ''} al ${moment(replacements.dateTo).format('D-MM-YYYY')}.xlsx`,
+                templateName: 'StockReportTemplate.xlsx'
+              }
+            }
+            case 'stockValue':
+            {
+              const replacements = {
+                dateFrom: options.dateFrom || null,
+                dateTo: options.dateTo || moment().endOf('day').toDate()
+              }
+              const [monthlyStockValues] = await sequelize.query(monthlyStockValue, { replacements })
+              const [dailyStockValues] = await sequelize.query(dailyStockValue, { replacements })
+
+              return {
+                sheets: [monthlyStockValues, dailyStockValues],
+                fileName: `Reporte de Stock Valorizado ${replacements.dateFrom ? `del ${moment(replacements.dateFrom).format('D-MM-YYYY')}` : ''} al ${moment(replacements.dateTo).format('D-MM-YYYY')}.xlsx`,
+                templateName: 'StockValueTemplate.xlsx'
+              }
+            }
+            default:
+              break;
           }
         } catch (error) {
           throw new BadRequest({ errors: error.message })
         }
       }
-	  },
+    },
     async (req, res) => {
-      const template = xlsx.readFile(path.resolve(__dirname, 'stock', 'StockReportTemplate.xlsx'), { cellStyles: true, sheetStubs: true })
-      const { sheets, fileName } = res.data
+      const { sheets, fileName, templateName } = res.data
+      const template = xlsx.readFile(path.resolve(__dirname, 'stock', templateName), { cellStyles: true, sheetStubs: true })
       sheets.forEach((data, index) => writeArrayToSheet({ sheet: template.Sheets[template.SheetNames[index]], array: data, startingAddress: { cell: 0, row: 1 } }))
-      
+
       const result = xlsx.write(template, { type: 'buffer', bookType: 'xlsx', bookSST: true })
       res.setHeader('Content-Type', 'application/vnd.openxmlformats')
       res.setHeader('Content-Disposition', 'attachment; filename=Report.xlsx')
